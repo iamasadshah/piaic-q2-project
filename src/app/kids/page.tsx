8c56be355533854e4764858fbf29b068de7707e3
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-09-29 11:34:57 +0500

    Mobile UI of Product Complete

--- src/app/kids/page.tsx
+++ src/app/kids/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getData, IProduct } from "@/lib/getData";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 const Page = async (): Promise<JSX.Element> => {
   const data = await getData();
@@ -13,18 +14,20 @@ const Page = async (): Promise<JSX.Element> => {
   return (
     <div>
       {filteredData.map((product: IProduct, index: number) => (
-        <div key={index} className="grid grid-cols-3">
-          <Card>
-            <h1>{product.title}</h1>
-            <p>{product.type}</p>
-            <p>{product.price}</p>
-            <p>{product.category.name}</p>
+        <div key={index} className="product">
+          <Card className="card">
             <Image
               src={product.urlImage}
               alt={product.title}
               width={100}
               height={100}
+              className="product-image"
             />
+            <h1 className="product-title">{product.title}</h1>
+            <p className="product-category">{product.category.name}</p>
+            <p className="product-type">{product.type}</p>
+            <p className="product-price">$ {product.price}</p>
+            <Button variant={"myButton"}>Get Info</Button>
           </Card>
         </div>
       ))}
